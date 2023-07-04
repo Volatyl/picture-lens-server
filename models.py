c2bf04f33159dfb2942c2fc65f051ffc4ecd7a02
Pat <wambuipatricia3@gmail.com>
2023-07-04 12:22:23 +0300

    Added models.py

---
+++ models.py
@@ -0,0 +1,44 @@
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import MetaData
+from sqlalchemy_serializer import SerializerMixin
+metadata = MetaData(naming_convention={
+    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+})
+
+
+db = SQLAlchemy(metadata=metadata)
+
+
+class User(db.Model, SerializerMixin):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String)
+    last_name = db.Column(db.String)
+    password = db.Column(db.String)
+
+
+class Image(db.Model, SerializerMixin):
+
+    __tablename__ = 'images'
+    id = db.Column(db.Integer, primary_key=True)
+    url = db.Column(db.String)
+    price = db.Column(db.Integer)
+    likes = db.Column(db.Integer)
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate=db.func.now())
+
+
+class Category(db.Model, SerializerMixin):
+    __tablename__ = 'categories'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    image_id = db.Column(db.Integer, db.ForeignKey('images.id'))
+
+    
+    image = db.relationship('Image', backref=db.backref('categories'))
+
+
+
+
+
+
