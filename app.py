4f5f1d7567a0b4b038d2e19e5e4583b334527eb9
Kennedy Muinde <kennedymuinde@gmail.com>
2023-07-04 22:57:01 +0300

    my commit

--- app.py
+++ app.py
@@ -6,5 +6,42 @@ from config import app, db, api
 from models import User, Image, Category
 
 
+class Index(Resource):
+    def get(self):
+        pass
+
+
+class Login(Resource):
+    def post(self):
+        pass
+
+
+class CheckSession(Resource):
+    def post(self):
+        pass
+
+
+class Logout(Resource):
+    def post(self):
+        pass
+
+
+class Edit(Resource):
+    def post(self):
+        pass
+
+    def patch(self):
+        pass
+
+    def delete(self):
+        pass
+
+
+api.add_resource(Index, '/index')
+api.add_resource(Login, '/login')
+api.add_resource(CheckSession, '/check_session')
+api.add_resource(Logout, '/logout')
+api.add_resource(Edit, '/edit')
+
 if __name__ == '__main__':
     app.run(debug=True, port=5555)
