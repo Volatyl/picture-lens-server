c92643bff632cc42bff714ff692c81573ae471b5
Kennedy Muinde <kennedymuinde@gmail.com>
2023-07-04 20:44:57 +0300

    Refactored configurations and added authentication

--- app.py
+++ app.py
@@ -1,55 +1,11 @@
 #!/usr/bin/env python3
 
-from flask import Flask, jsonify, make_response
-from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
+from flask import jsonify, make_response
 
-from models import db, User, Image,Category
+from models import db, User, Image, Category
 
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.json.compact = False
-
-migrate = Migrate(app, db)
-
-db.init_app(app)
-
-@app.route('/')
-def index():
-    return "<h1> PICTURE LENS </>"
-
-@app.route('/users',methods=['GET'])
-def get_users():
-    users=[]
-    for user in User.query.all():
-        user_dict = {
-            "id":user.id,
-            "first_name" : user.first_name,
-            "last_name" :user.last_name,
-            "password": user.password
-        }
-        users.append(user_dict)
-        response=make_response(jsonify(users),200)
-        response.headers["Content-Type"] = "application/json"
-
-        
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+from config import app
 
 
 if __name__ == '__main__':
-    app.run(debug=True,port=5555)
+    app.run(debug=True, port=5555)
--- models.py
+++ models.py
@@ -1,12 +1,7 @@
-from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import MetaData
+from sqlalchemy.ext.hybrid import hybrid_property
 from sqlalchemy_serializer import SerializerMixin
-metadata = MetaData(naming_convention={
-    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
-})
 
-
-db = SQLAlchemy(metadata=metadata)
+from config import db, bcrypt
 
 
 class User(db.Model, SerializerMixin):
@@ -14,10 +9,25 @@ class User(db.Model, SerializerMixin):
     id = db.Column(db.Integer, primary_key=True)
     first_name = db.Column(db.String)
     last_name = db.Column(db.String)
-    password = db.Column(db.String)
-    images= db.relationship('Image' ,backref = db.backref('users'))
+    username = db.Column(db.String)
+    _password_hash = db.Column(db.String)
 
+    images = db.relationship('Image', backref=db.backref('users'))
+
+    @hybrid_property
+    def password_hash(self):
+        return self._password_hash
+
+    @password_hash.setter
+    def password_hash(self, password):
+        password_hash = bcrypt.generate_password_hash(password.encode('utf-8'))
+        self._password_hash = password_hash.decode('utf-8')
+
+    def authenticate(self, password):
+        return bcrypt.check_password_hash(self._password_hash, password.encode('utf-8'))
     
+    def __repr__(self):
+        return f'User {self.username}, ID: {self.id}'
 
 
 class Image(db.Model, SerializerMixin):
@@ -29,10 +39,8 @@ class Image(db.Model, SerializerMixin):
     likes = db.Column(db.Integer)
     created_at = db.Column(db.DateTime, server_default=db.func.now())
     updated_at = db.Column(db.DateTime, onupdate=db.func.now())
-    user_id = db.Column(db.Integer,db.ForeignKey('user.id'))
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
     categories = db.relationship('Category', backref='image')
-    
-    
 
 
 class Category(db.Model, SerializerMixin):
@@ -40,11 +48,5 @@ class Category(db.Model, SerializerMixin):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
     image_id = db.Column(db.Integer, db.ForeignKey('images.id'))
-    user_id = db.Column(db.Integer,db.ForeignKey('user.id'))    
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
     images = db.relationship('Image', backref=db.backref('categories'))
-
-
-
-
-
-
