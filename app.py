15a446515808502751c60c70fa165192d2e5a7a4
Kennedy Muinde <kennedymuinde@gmail.com>
2023-07-05 13:43:12 +0300

    added some endpoints

--- app.py
+++ app.py
@@ -8,22 +8,58 @@ from models import User, Image, Category
 
 class Index(Resource):
     def get(self):
-        pass
+        images = [img.to_dict() for img in Image.query.all()]
+
+        return images, 200
+
+
+class Signup(Resource):
+    def post(self):
+        json = request.get_json()
+        user = User(
+            first_name=json['first_name'],
+            last_name=json['last_name'],
+            username=json['username'],
+            password_hash=json['password']
+        )
+        db.session.add(user)
+        db.session.commit()
+
+        session['user_id'] = user.id
+        return user.to_dict(), 201
+
+
+class CheckSession(Resource):
+    def get(self):
+        user_id = session['user_id']
+
+        if user_id:
+            user = User.query.filter_by(id=user_id).first()
+            return user.to_dict(), 200
+
+        return {}, 204
 
 
 class Login(Resource):
     def post(self):
-        pass
+        data = request.get_json()
+        username = data['username']
+        password = data['password']
 
+        user = User.query.filter_by(username=username).first()
 
-class CheckSession(Resource):
-    def post(self):
-        pass
+        if user:
+            user.authenticate(password)
+            session['user_id'] = user.id
+            return user.to_dict(), 200
+
+        return {'error': 'Invalid username or password'}, 401
 
 
 class Logout(Resource):
-    def post(self):
-        pass
+    def delete(self):
+        session['user_id'] = None
+        return {}, 204
 
 
 class Edit(Resource):
